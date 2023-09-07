a908956690c648a5239bd0dd253778bf63524cd0
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2023-09-07 16:57:02 +0800

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,38 @@
+local societyVeh = {}
+QBCore = exports['qb-core']:GetCoreObject()
 
+RegisterNetEvent("eric_qb_jobVehicle:returnVehicle", function(netID, plate)
+	local src = source
+	local vehicle = NetworkGetEntityFromNetworkId(netID)
+	local vehOwner = NetworkGetEntityOwner(vehicle)
+
+	if vehOwner and vehOwner ~= 0 then
+		TriggerClientEvent("eric_qb_jobVehicle:deleteVehicle", vehOwner, netID)
+	else
+		DeleteEntity(vehicle)
+		Wait(100)
+		if not DoesEntityExist(vehicle) then
+			societyVeh[plate] = nil
+		end
+	end
+end)
+
+RegisterNetEvent("eric_qb_jobVehicle:removeJobVehicle", function(plate)
+	societyVeh[plate] = nil
+end)
+
+QBCore.Functions.CreateCallback('eric_qb_jobVehicle:SpawnVehicle', function (source, cb, coords, model, plate)
+    local veh = QBCore.Functions.SpawnVehicle(source, model, coords, true)
+	local Player = QBCore.Functions.GetPlayer(source)
+    SetEntityHeading(veh, coords.w)
+    SetVehicleNumberPlateText(veh, plate)
+    local netId = NetworkGetNetworkIdFromEntity(veh)
+	societyVeh[plate] = Player.PlayerData.job.name
+    cb(netId)
+end)
+
+QBCore.Functions.CreateCallback("eric_qb_jobVehicle:checkPlateStatus", function(source, cb, plate)
+	cb(societyVeh[plate])
+end)
+
+print("This script created by AiReiKe\nThis script is for free")
